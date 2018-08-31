d325182f815bf83ad36ee143c36e8bd913783e6e
ayumitamai97 <ayumitamai97@gmail.com>
2018-08-31 17:39:15 +0900

    Improved validation

--- lib/tamai.rb
+++ lib/tamai.rb
@@ -6,16 +6,27 @@ require "pry"
 
 class Tamai
   def self.station(postal)
-    search(postal)
+    puts "数秒お待ちください"
+
+    postal_str = postal.to_s
+    return unless validate(postal_str)
+    search(postal_str)
+
+    if @station.nil?
+      puts "見つかりませんでした"
+      return
+    end
+
     name = @station["name"]
     kana = @station["kana"]
     line = @station["line"]
-    distance = @station["distance"]
+    distance = @station["distance"].to_i.to_s
     prev_station = @station["prev"]
     next_station = @station["next"]
     # prefecture = @station["prefecture"]
     # latitude = @station["y"]
     # longtitude = @station["x"]
+
     puts "最寄駅: " + line + " " + name + " （" + kana + "）駅"
     puts "隣駅: " + prev_station + "、" + next_station
     puts "ここから約" + distance + "m"
@@ -23,9 +34,30 @@ class Tamai
   
   private
   def self.search(postal)
-    postal_encoded = URI.encode postal.to_s
+    postal_encoded = URI.encode postal
     json = Net::HTTP.get(URI.parse("http://geoapi.heartrails.com/api/json?method=getStations&postal=#{postal_encoded}"))
     JSON.parse(json)
+
     @station = JSON.parse(json)["response"]["station"][0]
+  end
+
+  def self.validate(postal)
+    if correct?(postal)
+      return
+    elsif correct_containing_hyphen?(postal)
+      postal.gsub!("-", "")
+      return
+    else
+      puts "郵便番号を半角数字で入力してください"
+      return false
+    end
+  end
+
+  def self.correct?(postal)
+    /\d{3}-\d{4}/.match(postal)
+  end
+
+  def self.correct_containing_hyphen?(postal)
+    /\d{3}-\d{4}/.match(postal)
   end
 end
