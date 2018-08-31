b0e3dcb2aaec2897ac0992e12e1e516d88bc2892
ayumitamai97 <ayumitamai97@gmail.com>
2018-08-31 16:23:11 +0900

    Added new method station

--- lib/tamai.rb
+++ lib/tamai.rb
@@ -2,11 +2,28 @@ require "tamai/version"
 require "uri"
 require "net/http"
 require "json"
+require "pry"
 
 class Tamai
+  def self.station(postal)
+    search(postal)
+    puts @station
+    # @name = @station["name"]
+    # @kana = @station["kana"]
+    # @line = @station["line"]
+    # @latitude = @station["y"]
+    # @longtitude = @station["x"]
+    # @distance = @station["distance"]
+    # @prefecture = @station["prefecture"]
+    # @prev_@station = @station["prev"]
+    # @next_@station = @station["next"]
+  end
+  
+  private
   def self.search(postal)
     postal_encoded = URI.encode postal.to_s
-    json = Net::HTTP.get(URI.parse("http://geoapi.heartrails.com/api/json?method=getStations&postal=#{postal}"))
+    json = Net::HTTP.get(URI.parse("http://geoapi.heartrails.com/api/json?method=getStations&postal=#{postal_encoded}"))
     JSON.parse(json)
+    @station = JSON.parse(json)["response"]["station"][0]
   end
 end
--- lib/tamai/version.rb
+++ lib/tamai/version.rb
@@ -1,3 +1,3 @@
 class Tamai
-  VERSION = "0.1.4"
+  VERSION = "0.1.0"
 end
